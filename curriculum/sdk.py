557f0a113becc169285a53da0dcb715154234fbe
Ali Javani <aj.ali.javan@gmail.com>
2026-08-19 11:34:09 +0200

    feat: expand Excalidraw element and project APIs
    - Add new shape type checks to ExcalidrawElement
    - Introduce version property to ExcalidrawElement
    - Implement get_all_elements method in ExcalidrawProject

--- curriculum/sdk.py
+++ curriculum/sdk.py
@@ -42,12 +42,44 @@ class ExcalidrawElement:
     @property
     def stroke_color(self) -> str:
         return self._data.get("strokeColor", "")
+    
+    @property
+    def version(self) -> int:
+        return self._data.get("version", 0)
 
     def is_rectangle(self) -> bool:
         return self.type == "rectangle"
+    
+    def is_diamond(self) -> bool:
+        return self.type == "diamond"
 
     def is_ellipse(self) -> bool:
         return self.type == "ellipse"
+    
+    def is_arrow(self) -> bool:
+        return self.type == "arrow"
+    
+    def is_line(self) -> bool:
+        return self.type == "line"
+    
+    def is_freedraw(self) -> bool:
+        return self.type == "freedraw"
+    
+    def is_text(self) -> bool:
+        return self.type == "text"
+    
+    def is_image(self) -> bool:
+        return self.type == "image"
+    
+    def is_frame(self) -> bool:
+        return self.type == "frame"
+    
+    def is_embeddable(self) -> bool:
+        return self.type == "embeddable"
+    
+    def is_magicframe(self) -> bool:
+        return self.type == "magicframe"
+    
 
     def has_background_color(self, hex_color: str) -> bool:
         return self.background_color.lower() == hex_color.lower()
@@ -77,3 +109,6 @@ class ExcalidrawProject:
 
     def get_ellipses(self) -> list[ExcalidrawElement]:
         return [el for el in self._elements if el.is_ellipse()]
+    
+    def get_all_elements(self) -> list[ExcalidrawElement]:
+        return [el for el in self._elements]
